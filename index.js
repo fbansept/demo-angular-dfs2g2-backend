65244ccf7e0c65f2cbd3b783fa7bbcfc77308c6b
fbansept <bansept.franck@gmail.com>
2024-11-26 15:01:45 +0100

    preparation connexion

--- index.js
+++ index.js
@@ -2,25 +2,50 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors"); // Importer le middleware CORS
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Activer CORS pour toutes les requêtes
 app.use(cors());
+
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
+});
+
+connection.connect();
+
+connection.query("SELECT 1 + 1 AS solution", function (err, rows, fields) {
+  if (err) throw err;
+  console.log("The solution is: ", rows[0].solution);
+});
+
+connection.end();
 
 app.get("/", (req, res) => {
   res.send("Le serveur marche, mais il y a rien à voir ici");
 });
 
+app.post("/connexion", (req, res) => {
+  const jsonUtilisateur = req.body;
+  console.log(jsonUtilisateur);
+
+  res.json({ jwt: "le futur jwt" });
+});
+
 app.get("/categories", (req, res) => {
-  
-  const categoriesParDefaut =  [
+  const categoriesParDefaut = [
     { titre: "S", images: [] },
     { titre: "A", images: [] },
     { titre: "B", images: [] },
     { titre: "C", images: [] }
   ];
 
-  res.json(categoriesParDefaut)
-
+  res.json(categoriesParDefaut);
 });
 
 // Démarrer le serveur
